c3c855abf47503d57d9178c0363a004d4f518daa
Vladimir Quezada <a01254781@tec.mx>
2024-09-02 22:46:04 -0600

    Conditionals Practice

--- 09-practice-time-starting-setup/loops-in-action/loops-in-action.js
+++ 09-practice-time-starting-setup/loops-in-action/loops-in-action.js
@@ -9,11 +9,93 @@ function calculateSum() {
 
     outputValue = 0
 
-    for (let i = 0; i < enteredNumber; i++) {
-
+    for (let i = 0; i <= enteredNumber; i++) {
+        outputValue += i
     }
+    const outputResultElement = document.getElementById('calculated-sum')
 
-    console.log(enteredNumber)
+    outputResultElement.textContent = outputValue
+    outputResultElement.style.display = 'block'
 }
 
 calculateSumButtonElement.addEventListener('click', calculateSum)
+
+//highlighy links exercise
+
+const highlightLinksButtonElement = document.querySelector('#highlight-links button')
+
+function highlightLinks() {
+    const anchorElements = document.querySelectorAll('#highlight-links a')
+    for (const anchorElement of anchorElements) {
+        anchorElement.classList.add('highlight')
+    }
+}
+
+highlightLinksButtonElement.addEventListener('click', highlightLinks)
+
+//Display User data
+
+const dummyData = {
+    firstName: 'Vladimir',
+    lastName: 'Quezada',
+    age: 20
+}
+
+const displayUserDataButtonElement = document.querySelector('#user-data button')
+
+function displayUserData() {
+    const outputDataElement = document.getElementById('output-user-data')
+
+    outputDataElement.innerHTML = ''
+
+    for (const key in dummyData) {
+        const newUserDataListItemElement = document.createElement('li');
+        const outputText = key.toUpperCase() + ': ' + dummyData[key]
+        newUserDataListItemElement.textContent = outputText
+        outputDataElement.append(newUserDataListItemElement)
+    }
+}
+
+displayUserDataButtonElement.addEventListener('click', displayUserData)
+
+//statistics / Roll The Dice
+
+const rollDiceBtnElement = document.querySelector('#statistics button')
+
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1
+}
+
+function deriveNumberofDiceRolls() {
+    const targetNumberInputElem = document.getElementById('user-target-number')
+    const diceRollsListElem = document.getElementById('dice-rolls')
+
+    const enteredNumber = targetNumberInputElem.value
+    diceRollsListElem.innerHTML = ''
+
+    let hasRolledTargetNum = false
+    let numOfRolls = 0
+
+    const outputTotalRollsElem = document.getElementById('output-total-rolls')
+    const outputTargetNumberElem = document.getElementById('output-target-number')
+
+    while (!hasRolledTargetNum) {
+        const rolledNum = rollDice()
+        // if(rolledNum == enteredNumber){
+        //     hasRolledTargetNum = true
+        // }
+
+        numOfRolls++
+        const newRollListElem = document.createElement('li')
+        const outputText = 'Roll ' + numOfRolls + ': ' + rolledNum
+        newRollListElem.textContent = outputText
+        diceRollsListElem.append(newRollListElem)
+        hasRolledTargetNum = rolledNum == enteredNumber
+        outputTotalRollsElem.textContent = numOfRolls
+    }
+
+    outputTargetNumberElem.textContent = enteredNumber
+
+}
+
+rollDiceBtnElement.addEventListener('click', deriveNumberofDiceRolls)
