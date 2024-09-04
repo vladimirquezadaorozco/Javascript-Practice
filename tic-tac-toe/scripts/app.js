a6312e84d75fe7f0eeb23701fc832331721dcf4a
Vladimir Quezada <a01254781@tec.mx>
2024-09-03 19:26:11 -0600

    Storing and Managing Submitted Data

--- tic-tac-toe/scripts/app.js
+++ tic-tac-toe/scripts/app.js
@@ -1,6 +1,20 @@
+let editedPlayer = 0
+
+const players = [
+    {
+        name: '',
+        symbol: 'X'
+    },
+    {
+        name: '',
+        symbol: 'O'
+    },
+]
+
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
+const errorsOutputElement = document.getElementById('config-errors')
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
--- tic-tac-toe/scripts/config.js
+++ tic-tac-toe/scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+    editedPlayer = +event.target.dataset.playerid
     playerConfigOverlayElement.style.display = 'block';
     backdropElement.style.display = 'block';
 }
@@ -6,11 +7,26 @@ function openPlayerConfig() {
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error')
+    errorsOutputElement.textContent = ''
+    formElement.firstElementChild.lastElementChild.value = ''
 }
 
 function savePlayerConfig(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const enteredPlayername = formData.get('playername');
-    console.log(enteredPlayername);
+    const enteredPlayername = formData.get('playername').trim();
+
+    if (!enteredPlayername) {
+        event.target.firstElementChild.classList.add('error')
+        errorsOutputElement.textContent = 'Please enter valid name'
+        return
+    }
+
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data')
+    updatedPlayerDataElement.children[1].textContent = enteredPlayername
+
+    players[editedPlayer - 1].name = enteredPlayername
+
+    closePlayerConfig()
 }
