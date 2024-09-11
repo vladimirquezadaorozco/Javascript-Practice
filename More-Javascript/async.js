9da59bb3c2b1a9ea8241c2edbe7235c1e9bc07b7
Vladimir Quezada <a01254781@tec.mx>
2024-09-11 01:35:12 -0600

    Promises

--- More-Javascript/async.js
+++ More-Javascript/async.js
@@ -1,12 +1,23 @@
-const fs = require('fs')
+const fs = require('fs/promises')
 
 function readFile() {
     let fileData
-    fileData = fs.readFile('data.txt', function (error, fileData) {
-        console.log('File parsing done')
-        console.log(fileData.toString())
-    })
-
+    // fileData = fs.readFile('data.txt', function (error, fileData) {
+    // if(error){
+    //     ...
+    // }
+    //     console.log('File parsing done')
+    //     console.log(fileData.toString())
+    // })
+    fileData = fs.readFile('data.txt')
+        .then(function () {
+            console.log('File parsing done')
+            console.log(fileData.toString())
+        })
+        .then(function () { })
+        .catch(function (error) {
+            console.log(error)
+        })
 
     console.log('Hi there')
 }
